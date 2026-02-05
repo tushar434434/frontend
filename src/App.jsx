05fbb24032451bd74db0f53eeeeb57f875aba8e1
Tushar Kumar <tusharkumar1036@gmail.com>
2026-02-05 14:13:20 +0530

    initial react project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 //import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Heade.jsx";
 
 function App() {
   return (
    <>
+   <Header/>
    <h1>hello tushar</h1>
    <h2>yes yes</h2>
    </>
---
+++ src/components/Heade.jsx
@@ -0,0 +1,7 @@
+function Heade()
+{
+    return(
+        <h1>welcome to the first page of react app</h1>
+    )
+}
+export default  Heade;
